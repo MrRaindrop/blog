a77201b17864671def631e04a73e60d2322913ce
_danZ <tekkahs@gmail.com>
2017-11-19 23:29:42 +0800

    update

---
+++ app.js
@@ -0,0 +1,11 @@
+var spawn = require('child_process').spawn;
+free = spawn('hexo', ['server', '-p 3000']);/* 其实就是等于执行hexo server -p 4000*/
+free.stdout.on('data', function (data) {
+  console.log('standard output:\n' + data);
+});
+free.stderr.on('data', function (data) {
+  console.log('standard error output:\n' + data);
+});
+free.on('exit', function (code, signal) {
+  console.log('child process eixt ,exit:' + code);
+});
